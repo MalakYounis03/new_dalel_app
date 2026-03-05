9cd3fae7134c54caf63d096c64e955f6324b5e1c
MalakYounis03 <ibrhr55@gmail.com>
2026-03-05 13:20:42 +0200

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:dalel_app/app/core/app_colors.dart';
 import 'package:dalel_app/app/core/database/cache/cache_helper.dart';
 import 'package:dalel_app/app/core/service/service_locator.dart';
+import 'package:dalel_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   setupLocator();
   await getIt<CacheHelper>().init();
   runApp(const MyApp());
